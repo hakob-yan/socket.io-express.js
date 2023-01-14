3410a3ad7973f596f71ebaa4017d35d25a33bea7
hakob-yan <hakob9735@gmail.com>
2023-01-14 14:54:40 +0400

    seperated npm to client and server

--- server/app.js
+++ server/app.js
@@ -24,6 +24,8 @@ app.use('/', routes.home)
 
 
 const server = http.createServer(app);
-const io = require('socket.io')(server)
+const io = require('socket.io')(server,{
+    cors:{origin:['http://127.0.0.1:5500/']}
+})
 io.on('connection', client => { console.log(client.id); });
 server.listen(3000)
